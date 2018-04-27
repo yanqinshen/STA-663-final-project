24870e8102c0a925aba687338fec734888e92f34
Xuetong Li <xl222@duke.edu>
2018-04-27 08:49:36 -0400

    fix path

--- examples/BreastCancer.py
+++ examples/BreastCancer.py
@@ -2,7 +2,7 @@ from biclustering.functions import SSVD_python
 from biclustering.functions import plotClusters
 import pandas as pd
 
-OutData = pd.read_csv('data/BreastCancerData.txt', sep=' ', header=0)
+OutData = pd.read_csv('../data/BreastCancerData.txt', sep=' ', header=0)
 X_Out = np.array(OutData.T)
 u, s, v, niter = SSVD_python(X_Out)
 groups = pd.read_csv('data/BreastCancerLabels.txt', sep=' ', header = -1)
